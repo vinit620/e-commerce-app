db3cb30a2f80a610871fd83549d11f0f1e0e079d
Vinit Jha <jhavinit01@gmail.com>
2023-01-02 15:41:36 +0530

    DB Connected

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
+require('dotenv').config()
 const express = require('express');
-const app = express();
-const PORT = 3000;
+const mongoose = require('mongoose');
+mongoose.set("strictQuery", false);
 
-app.get('/', (req, res) => res.send('Hello, World!'));
+mongoose
+    .connect(process.env.DATABASE_URL)
+    .then(() => console.log('DB CONNECTED'))
+    .catch(() => console.log('ERROR CONNECTING DB'));
+
+const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, console.log(`Server started at port ${PORT}`));
