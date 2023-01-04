10d94932fb1657aa05b00c053e3f33fb5033288b
Vinit Jha <jhavinit01@gmail.com>
2023-01-04 10:18:07 +0530

    completed user signin

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,7 @@
 const User = require('../models/user');
 const { validationResult } = require('express-validator');
+const jwt = require('jsonwebtoken');
+const expressJwt = require('express-jwt');
 
 
 // USER REGISTRATION
@@ -28,6 +30,52 @@ exports.signup = (req, res) => {
             id: user._id,
             name: user.name,
             email: user.email,
+        });
+    });
+};
+
+
+// USER SIGNIN
+exports.signin = (req, res) => {
+
+    // validation error (if any)
+    const val_error = validationResult(req);
+
+    if (!val_error.isEmpty()) {
+        return res.status(422).json({
+            error_param: val_error.array()[0].param,
+            error_msg: val_error.array()[0].msg
+        })
+    }
+
+    // Extract email and password from req
+    const { email, password } = req.body;
+
+    // Find user in DB
+    User.findOne({ email }, (err,user) => {
+        if (err || !user) {
+            return res.status(400).json({
+                error: 'User does not exist'
+            });
+        }
+
+        if (!user.autheticate(password)) {
+            return res.status(401).json({
+                error: 'Incorrect password'
+            });
+        }
+
+        // Create token
+        const authToken = jwt.sign({_id: user._id}, process.env.TOKEN_STRING);
+
+        // Put token in cookie
+        res.cookie('authToken', authToken, {expire: new Date() + 9999});
+
+        // send response to frontend
+        const { _id, name, email, role } = user;
+        return res.json({
+            authToken,
+            user: { _id, name, email, role }
         });
     });
 };
--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const { body } = require('express-validator');
-const { signout, signup } = require('../controllers/auth');
+const { signup, signin } = require('../controllers/auth');
 const router = express.Router();
 
 // User Registration
@@ -11,6 +11,16 @@ router.post(
         body('password').isLength({ min: 5 }) // password validation
     ],
     signup
+);
+
+// User Signin
+router.post(
+    '/signin',
+    [
+        body('email').isEmail(),  // email validation
+        body('password').isLength({ min: 5 }) // password validation
+    ],
+    signin
 );
 
 
